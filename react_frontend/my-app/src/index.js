ac1cbbd22d9417a2de7bbca35a884fac7dbf8092
akmurphak <114615606+amandamurphy2025@users.noreply.github.com>
2025-03-29 17:50:02 -0500

    new index.js

--- react_frontend/my-app/src/index.js
+++ react_frontend/my-app/src/index.js
@@ -15,7 +15,7 @@ root.render(
         authorizationParams={{
             redirect_uri: window.location.origin,
             audience: "https://pixel-pose.us.auth0.com/api/v2/",
-            scope: "read:current_user update:current_user_metadata"
+            scope: "openid profile email read:current_user update:current_user_metadata"
         }}
     >
         <React.StrictMode>
