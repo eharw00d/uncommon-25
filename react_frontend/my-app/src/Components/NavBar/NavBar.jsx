de22c3f123fc7265ccbf33b93a729393dbce878b
akmurphak <114615606+amandamurphy2025@users.noreply.github.com>
2025-03-29 16:22:10 -0500

    Login auth

--- react_frontend/my-app/src/Components/NavBar/NavBar.jsx
+++ react_frontend/my-app/src/Components/NavBar/NavBar.jsx
@@ -4,13 +4,14 @@ import './NavBar.css'
 import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
+    const { loginWithRedirect } = useAuth0();
   return (
     <div className="navbar">
       <div className="navbar-title">
         <Link to='/'><span>_Pixel</span>pose</Link>
       </div>
       <div className="login-link">
-        <Link to="/login"><button>Login</button></Link>
+        <button onClick={() => loginWithRedirect()}>Login</button>
       </div>
     </div>
   )
