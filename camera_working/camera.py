6998cb2283247535e2b289b29696033babceb0f5
Eric Harwood <eharwood@uchicago.edu>
2025-03-29 12:29:21 -0500

    Update camera.py

--- camera_working/camera.py
+++ camera_working/camera.py
@@ -1,18 +1,28 @@
 import os, sys, shutil, subprocess
 import cv2
 from matplotlib import pyplot as plt
-
+import cv2
 
 
 def camera_capture():
-    camera = cv2.VideoCapture(0)
 
-    #get a frame from the camera
-    while camera.isOpened():
-        ret, frame = camera.read()
-        cv2.imshow('Webcam', frame)
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame.")
+            break
 
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-if __name__ == "__main__":\
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
     camera_capture()
